81a1765bea3a8f9aa9bbe3e3d8d0c16b3d2b0b48
Christian Baker <christianbaker1998@gmail.com>
2020-11-13 09:12:05 -0500

    udated user list with edit

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user.class';
+import { SyncAsync } from '@angular/compiler/src/util';
+
+const baseurl: string = "http://localhost:50455/api/users";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient 
+    ) { }
+
+    list(): Observable<User[]> {
+      return this.http.get(`${baseurl}`) as Observable<User[]>;  
+    } 
+    get(id: number): Observable<User> {
+      return this.http.get(`${baseurl}/${id}`) as Observable<User>;
+    }
+
+    change(user: User): Observable<any> {
+      return this.http.put(`${baseurl}/${user.id}`, user) as Observable<any>;
+    }
+    
+    }
+    2
+
